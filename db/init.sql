9222fce8bc9ee5fd53e1419c4a116a1a0fde47c7
woooyoung <axdsw121@gmail.com>
2024-10-22 15:47:58 +0900

    initData 기반, JPA 기반, AppConfig 기반

---
+++ db/init.sql
@@ -0,0 +1,5 @@
+# 개발 DB 생성
+
+DROP DATABASE IF EXISTS SpringBatch_app_10__dev;
+CREATE DATABASE SpringBatch_app_10__dev;
+USE SpringBatch_app_10__dev;
---
+++ src/main/java/com/koreait/exam/springbatch_app_10/app/base/initData/InitDataBefore.java
@@ -0,0 +1,7 @@
+package com.koreait.exam.springbatch_app_10.app.base.initData;
+
+public interface InitDataBefore {
+    default void before() {
+
+    }
+}
