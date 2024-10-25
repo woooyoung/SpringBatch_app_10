e3c0ec4777d60a2c71de750a7a42b633ae4935ad
woooyoung <axdsw121@gmail.com>
2024-10-25 11:10:31 +0900

    입력된 연 월에 해당하는 날짜 범위 구하기

--- src/main/java/com/koreait/exam/springbatch_app_10/app/calculate/controller/AdmCalculateController.java
+++ src/main/java/com/koreait/exam/springbatch_app_10/app/calculate/controller/AdmCalculateController.java
@@ -1,9 +1,14 @@
 package com.koreait.exam.springbatch_app_10.app.calculate.controller;
 
+import com.koreait.exam.springbatch_app_10.util.Ut;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.time.LocalDateTime;
 
 @Controller
 @RequestMapping("/adm/calculate")
@@ -12,5 +17,17 @@ public class AdmCalculateController {
     @PreAuthorize("hasAuthority('ADMIN')")
     public String showMakeData() {
         return "adm/calculate/makeData";
+    }
+
+    @PostMapping("/makeData")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    @ResponseBody
+    public String makeData(String yearMonth) {
+        int monthEndDay = Ut.date.getEndDayOf(yearMonth);
+        String fromDateStr = yearMonth + "-01 00:00:00.000000";
+        String toDateStr = yearMonth + "-%02d 23:59:59.999999".formatted(monthEndDay);
+        LocalDateTime fromDate = Ut.date.parse(fromDateStr);
+        LocalDateTime toDate = Ut.date.parse(toDateStr);
+        return "fromDateStr : %s<br>toDateStr : %s".formatted(fromDateStr, toDateStr);
     }
 }
